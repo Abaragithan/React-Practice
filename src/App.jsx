fc6013bee4a3393c51206bd271bc2ba4e1aff921
abaragithan <abaragithan02@gmail.com>
2025-02-11 03:56:07 +0530

    useState learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import User from './User'
 
 
 const App = () => {
 
-  const person = {
-    name: "abaragithan",
-    age: 25,
-    email: "abaragithan02@gmail.com",
+  // const person = {
+  //   name: "abaragithan",
+  //   age: 25,
+  //   email: "abaragithan02@gmail.com",
+  // }
+
+  const [num, setNum] = useState(1);
+
+  const clickEvent = () => {
+    setNum(num + 1);
+    console.log(num);
   }
 
   return (
     <div>
-      App
-      <User
-        {...person}
-      />
+      <h1>{num}</h1>
+      <button onClick={clickEvent}>Add</button>
+
     </div>
   )
 }
